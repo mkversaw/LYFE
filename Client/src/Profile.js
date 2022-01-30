13f7ebe4ac8d49f327abf1a1fce38af79e721cd1
mkversaw <mkversaw@gmail.com>
2022-01-30 11:36:36 -0600

    final final build done v1.0 last commit officially

--- Client/src/Profile.js
+++ Client/src/Profile.js
@@ -17,10 +17,10 @@ const Profile = () =>{
         
         <div className = "stats" >
             
-            <ul><li><h1> Level: 3 </h1> </li>
-            <li><h1> XP: 500 </h1></li>
-            <li><h1> Coins: 453 </h1></li>
-            <li><h1> Savings: $0.32 / month </h1></li></ul>
+            <ul><li><h1> Level: 0 </h1> </li>
+            <li><h1> XP: 0 </h1></li>
+            <li><h1> Coins: 0 </h1></li>
+            <li><h1> Savings: $0 / month </h1></li></ul>
         </div>
         </div>
     )
