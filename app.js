9098558baddb67690878f2a9395dbd2af17b0405
mkversaw <mkversaw@gmail.com>
2022-01-29 18:01:53 -0600

    basic login/register w/ firebase

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+require('dotenv').config()
 const ejs = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
 const { Schema } = mongoose; // easier to make new schemas
 const https = require('https');
 const http = require('http');
+const admin = require('firebase-admin');
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+var mongooseConn = mongoose.connection;
+
+mongooseConn.on('connected', function() {
+	console.log('connected to mongoDB successfully');
+});
+
+mongooseConn.on('disconnected', function() {
+	console.log('disconnected from mongoDB successfully');
+});
+
+mongooseConn.on('error', console.error.bind(console, 'connection error:'));
+module.exports = mongooseConn; // ???
 
 const app = express();
 
@@ -12,23 +29,119 @@ app.use(express.static("public"));
 
 const { initializeApp } = require('firebase/app');
 const { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } = require('firebase/auth');
+const internal = require('stream');
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAx4MvE-Zo4QquSyTC6nqml7IPpYsDUs0Y",
-	authDomain: "lyfetamuhack22.firebaseapp.com",
-	projectId: "lyfetamuhack22",
-	storageBucket: "lyfetamuhack22.appspot.com",
-	messagingSenderId: "417014250599",
-	appId: "1:417014250599:web:5caa7690dcc0f06cefa8c2",
-	measurementId: "G-M26TQ1EK64"
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE,
+	messagingSenderId: process.env.FIREBASE_MESSAGING,
+	appId: process.env.FIREBASE_APP_ID,
+	measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
+
+const userSchema = new mongoose.Schema ({
+	id: String,
+	name: String,
+	goldcoin: Number,
+	socialcredit: Number
+});
+
+const User = mongoose.model("users", userSchema);
 
 initializeApp(firebaseConfig);
 
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.render("login.ejs");
 });
+
+app.get("/register", function(req, res) {
+	const email = req.body.email;
+	const password = req.body.pass1;
+	const password2 = req.body.pass2;
+	const name = req.body.username;
+  
+	if(password != password2) {
+		res.redirect("/register");
+		return;
+	}
+
+	const auth = getAuth();
+	signInWithEmailAndPassword(auth, email, password) // check if account already exists
+	  .then((userCredential) => {
+		// Signed in
+		const user = userCredential.user;
+		User.findOne({id: user.uid}, function(err, foundUser) {
+		  if (err) {
+			res.redirect("/register");
+		  } else {
+			if (foundUser) {
+			  res.redirect("/mainpage");
+			}
+		  }
+		});
+		
+	  })
+	  .catch((error) => { // no existing user
+		const auth = getAuth();
+		createUserWithEmailAndPassword(auth, email, password)
+		  .then((userCredential) => {
+			// Signed in
+			const user = userCredential.user;
+			const newUser = new User({
+			  id: user.uid,
+			  name: name,
+			  goldcoin: 0,
+			  socialcredit: 0
+			});
+			newUser.save(function(err) {
+			  if (err) {
+				res.redirect("/register");
+			  }
+			  res.cookie('uid', user.uid);
+			  res.redirect("/mainpage");
+			});
+		  })
+		  .catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			console.log(errorCode, errorMessage);
+			res.redirect("/register");
+		  });
+	  });
+});
+
+app.post("/login", function(req, res){
+	const email = req.body.username;
+	const password = req.body.password;
+	let foundUsername = "";
+  
+	const auth = getAuth();
+	signInWithEmailAndPassword(auth, email, password)
+	  .then((userCredential) => {
+		// Signed in
+		const user = userCredential.user;
+		User.findOne({id: user.uid}, function(err, foundUser) {
+		  if (err) {
+			res.redirect("/login");
+		  } else {
+			if (foundUser) {
+			  res.cookie('uid', user.uid);
+			  res.redirect("/mainpage");
+			}
+		  }
+		});
+
+	  })
+	  .catch((error) => {
+		const errorCode = error.code;
+		const errorMessage = error.message;
+		console.log(errorCode, errorMessage);
+		res.redirect("/login");
+	  });
+  });
 
 let port = 3000;
 
